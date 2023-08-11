2cd73d3df65d5ec682f2df0ff0acb9ef1fb41616
wanguilindsey <wanguilindsey@gmail.com>
2023-08-11 13:55:19 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *  Return: Zero value
+ */
+
+int main(void)
+{
+	char c;
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}

44775ecdcb230de1a34a5fc27afb59e70a0abe33
wanguilindsey <wanguilindsey@gmail.com>
2023-08-11 14:19:05 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Return: Zero value
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j;
+	int count = 0;
+
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (count != 4949)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+			count++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -10,3 +10,4 @@ program that prints all the numbers of base 16 in lowercase, followed by a new l
 program that prints all possible combinations of single-digit numbers
 rogram that prints all possible different combinations of two digits
 program that prints all possible different combinations of three digits
+program that prints all possible combinations of two two-digit numbers

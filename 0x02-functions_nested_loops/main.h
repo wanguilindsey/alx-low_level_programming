b58dffac18ee569f5eb4fcf74c3d8a63aa572087
wanguilindsey <wanguilindsey@gmail.com>
2023-08-15 12:31:38 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,13 @@
 #ifndef main_h
 #define main_h
 
-int_putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
-int_abs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);

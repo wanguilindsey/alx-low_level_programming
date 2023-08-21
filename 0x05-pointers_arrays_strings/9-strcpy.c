e8ff5ce14672b8e3d1ffe0c1362e529e1dcfa33f
wanguilindsey <wanguilindsey@gmail.com>
2023-08-21 12:21:25 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,17 +9,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int l = 0;
-	int x = 0;
-
-	while (*(src + 1) != '\0')
+	int i;
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		l++;
+		dest[i] = src[i];
 	}
-	for ( ; x < 1 ; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[l] = '\0';
+	dest[i++] = '\0';
 	return (dest);
 }

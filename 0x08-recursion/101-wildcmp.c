5bae0f91fc2fb0507bb938881c3373f379ba9c99
wanguilindsey <wanguilindsey@gmail.com>
2023-08-30 08:49:13 -0700

    Inception

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * wildcmp - checks if two strings are identical
+ * @s1: char to check
+ * @s2: char to check
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}

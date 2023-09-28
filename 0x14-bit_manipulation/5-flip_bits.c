b430cad1cca8459cb97da877d89581c3d606967c
wanguilindsey <wanguilindsey@gmail.com>
2023-09-28 03:37:35 -0700

    counts no. of flips

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits to flip
+ * to get from one number to another
+ * @n: first no.
+ * @m: second no.
+ * Return: count of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int j, count = 0;
+	unsigned long int existing;
+	unsigned long int x = n ^ m;
+
+	for (j = 63; j >= 0; j--)
+	{
+		existing = x >> j;
+		if (exisitng & 1)
+			count++;
+	}
+
+	return (count);
+}

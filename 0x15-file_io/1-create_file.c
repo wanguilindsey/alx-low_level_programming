b65b0adcfa8b6d539019a19a1d16b58db664e038
wanguilindsey <wanguilindsey@gmail.com>
2023-10-02 10:56:23 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure 
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int x, y, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	y = write(fd, text_content, len);
+
+	if (x == -1 || y == -1)
+		return (-1);
+	close(x);
+	return (1);
+}

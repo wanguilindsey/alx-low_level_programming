ee5a4b0a87b90e30e1742a49e5f983a717ba471f
wanguilindsey <wanguilindsey@gmail.com>
2023-10-02 10:59:02 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	y = write(fd, text_content, len);
+	y = write(x, text_content, len);
 
 	if (x == -1 || y == -1)
 		return (-1);
